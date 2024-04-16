1c5a09cd619479a249770b0e4b4c5a8167266d0e
christy james <jkuttu@gmail.com>
2024-04-16 19:29:04 +0530

    Game over minimalistic

--- Common.gd
+++ Common.gd
@@ -2,3 +2,5 @@ extends Node
 var player
 var score:int= 0 
 signal ScoreSignal(score:int)
+var playerHealth:int = 3
+signal GameOver()
--- Models/Basics.gd
+++ Models/Basics.gd
@@ -49,5 +49,10 @@ func _process(delta):
 
 func _on_area_2d_body_entered(body):
 	print(body.name)
+	if not body.is_in_group("enemy"):
+		return
+	Common.playerHealth-=1
+	if Common.playerHealth==0:
+		print("Game Over")
 	body.queue_free()
 	pass # Replace with function body.
